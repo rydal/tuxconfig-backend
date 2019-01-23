bef69eb37752a257aaee5fd1088eeb8250dc8b24
Rob Brew <rb602@kent.ac.uk>
2019-01-23 08:59:28 +0000

    Update CustomDataSource.java

--- src/jsplink/CustomDataSource.java
+++ src/jsplink/CustomDataSource.java
@@ -4,13 +4,6 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 public class CustomDataSource {
    // JDBC driver name and database URL
-
-   static final String DB_URL = "jdbc:mysql://localhost/linuxconf";
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-
-   //  Database credentials
-   static final String USER = "arwen";
-   static final String PASS = "imleaving";
    private static DataSource datasource;
    private static final BasicDataSource basicDataSource;
 
